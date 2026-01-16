cd6e669e14577f96bbbba0ff0aa19d2e8c99b513
CesareFortunato <cesare.fortunato89@gmail.com>
2026-01-16 17:26:12 +0100

    feat: first setup header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,10 +1,28 @@
+import logo from '../assets/dc-logo.png'
+
 function MainHeader() {
 
 
   return (
+    <header>
    <figure>
-    <img src="" alt="" />
+    <img src={logo} alt="" />
    </figure>
+   <nav>
+    <ul>
+      <li>CHARACTERS</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
+    </ul>
+   </nav>
+   </header>
   )
 }
 
