974cb18f4e87310003ae9c887fd23a26a1748d04
CesareFortunato <cesare.fortunato89@gmail.com>
2026-01-17 12:21:46 +0100

    feat: setup footer

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,9 +1,35 @@
+import footerBg from '../assets/footer-bg.jpg';
+import footerLogo from '../assets/dc-logo-bg.png'
+
 function MainFooter() {
 
 
-  return 
-    
-  
+  return (
+
+    <>
+      <div className="container-footer">
+        
+<ul className='list-footer'>
+  <h2 className='footer-titles'>DC COMICS</h2>
+  <li className='el-footer'>ciao</li>
+  <li className='el-footer'>ciao</li>
+  <li className='el-footer'>ciao</li>
+  <li className='el-footer'>ciao</li>
+  <li className='el-footer'>ciao</li>
+  <li className='el-footer'>ciao</li>
+</ul>
+
+
+        <figure>
+          <img src={footerLogo} alt="" />
+        </figure>
+      </div>
+
+
+    </>
+
+  )
+
 }
 
 export default MainFooter
