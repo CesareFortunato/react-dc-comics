96fdab3b3169c37c236e709144493b85be3afd8a
CesareFortunato <cesare.fortunato89@gmail.com>
2026-01-20 16:00:22 +0100

    feat: create card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card(props) {
+    return (
+        <div className="card">
+            <img src={props.image} alt={props.title} />
+            <h3>{props.title}</h3>
+        </div>
+
+    )
+}
+
+export default Card
