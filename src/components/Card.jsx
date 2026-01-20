6ecd5bf090f16cf8ecd0d02c11e1d167b927d048
CesareFortunato <cesare.fortunato89@gmail.com>
2026-01-20 17:23:54 +0100

    feat: deconstruct card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,20 @@
+
+
 export default function Card(props) {
+
+    const {title,image} = props
+
   return (
     <article className="card">
       <div className="card-media">
         <img
-          src={props.image}
-          alt={props.title}
+          src={image}
+          alt= {title}
         />
       </div>
 
       <h3 className="card-title">
-        {props.title}
+        {title}
       </h3>
     </article>
   )
